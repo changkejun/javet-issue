6152bd81f81f8c1c028c94499210fa56495e842a
Chang Kejun <changkejun@hotmail.com>
2025-01-23 10:43:14 +0900

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+set path=C:\EFW_ALL\jdk-23\bin;
+rem set CLASSPATH=C:\EFW_ALL\pleiades\workspace\javetTest\bin;C:\EFW_ALL\pleiades\workspace\javetTest\lib\javet-3.1.8.jar;
+set CLASSPATH=C:\EFW_ALL\pleiades\workspace\javetTest\bin;C:\EFW_ALL\pleiades\workspace\javetTest\lib\javet-4.1.1.jar;C:\EFW_ALL\pleiades\workspace\javetTest\lib\javet-node-windows-x86_64-4.1.1.jar;C:\EFW_ALL\pleiades\workspace\javetTest\lib\javet-v8-windows-x86_64-4.1.1.jar
+java Test
+
+pause
